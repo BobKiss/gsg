39e716bf1e2bec6a934f152b0c339244d9282d5a
Bodya <ddrigass@gmail.com>
2019-08-29 17:05:56 +0300

    Add images and add frontend in About us page

--- themes/gsgsite/templates/AboutUs.php
+++ themes/gsgsite/templates/AboutUs.php
@@ -8,7 +8,35 @@ Template Name: About Us
 <div class="page">
 
   <div class="aboutUsPageWrapper">
-    
+    <section class="aboutUsFirstScreen" style="background-image: url(<?php echo get_bloginfo('url') ?>/wp-content/uploads/2019/08/aboutUsBg.jpg)">
+      <a href="<?php echo get_bloginfo('url') ?>" class="logo">
+        <img src="<?php echo get_bloginfo('url') ?>/wp-content/uploads/2019/08/mainLogo.png" alt="<?php echo get_bloginfo('name') ?>">
+      </a>
+      <div class="borderBlock container">
+        <div class="row"></div>
+        <div class="row">
+          <div class="title">Who we are <br> What we do</div>
+        </div>
+        <div class="row"></div>
+        <img class="decorCircle" src="<?php echo get_bloginfo('url') ?>/wp-content/uploads/2019/08/decorAbout.png" alt="decorAbout">
+      </div> <!--.borderBlock-->
+    </section> <!-- .aboutUsFirstScreen-->
+
+    <section class="aboutYellow">
+      <div class="titleContainer">
+        <div class="title">Shaham Crystal Group | Company profile</div>
+      </div>
+      <div class="container">
+        <div class="part">
+          <p>קבוצת גביש שחם היא חברה יזמית בתחום הנדל"ן המתמחה בתכנון ובניית פרויקטים קונספטואליים ייחודיים, מעוצבים ומותאמים אישית. בפרויקטים שאנו בונים אנו מיישמים את תפיסת העולם היסודית שלנו, "החיים קצרים" ו"חיים רק פעם אחת", וממצים מהחיים את כל הטוב שיש. מסיבה זו, אנו דוגלים בארכיטקטורה ועיצוב מוקפדים, חדשניים ויצירתיים ובמפרטים מפנקים וחלומיים. אנחנו מעניקים ללקוחותינו יותר מקירות מעוצבים ומפרטים יוקרתיים, את הבתים שלנו אנו יוצרים עם נשמה, ומתאימים אותם אישית לסיפור ולעולם הפרטי של כל אחד מלקוחותינו. לחלומות, לאהבות, לתחביבים ולפנטזיות שרק תעזו לחלום.</p>
+          <p>אנו לא נרתעים מפרויקטים מורכבים. חילוץ פרויקטים מקשיים, הסרת חסמים, מימון, ופתרון מחלוקות משפטיות, אצלנו זו שיגרה.  אנחנו אלופים בהכנת לימונדה! לוקחים פרויקטים מסובכים משפטית ותכנונית, עם קונפליקטים וחסמים רבים, ומצליחים להפוך את הלימון החמוץ ללימונדה מתוקה ורעננה.</p>
+        </div>
+        <div class="part">
+          <p>את כל זה אנו מצליחים לעשות בזכות צוות הקומנדו הייחודי שלנו, אשר פיתח מומחיות רבה בפיתוח פרויקטים משלב שינוי/ תכנון התב"ע, דרך פתרון כל החסמים התכנוניים הבירוקרטיים והמשפטיים ועד למסירת המוצר המוגמר והמותאם אישית לכל לקוח.</p>
+          <p>תפיסת העולם שלנו ואת העיצובים הייחודיים שלנו לכל מקום אליו אנו מגיעים. כל הפרויקטים שלנו, ללא קשר לגודלם, זוכים לטיפול אישי וליווי צמוד של עורך דין, מהנדס ומנהל פרויקט. אשר מובילים את הפרויקט מרגעיו הראשונים ועד למסירתו ללקוח.</p>
+        </div>
+      </div>
+    </section>
   </div>
 
 </div>
