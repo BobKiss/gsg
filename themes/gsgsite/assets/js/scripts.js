43f9635c09eccf424183fb6c73fa53fdf6715ee6
Dogrof <Zeron1@bk.ru>
2019-08-30 10:08:28 +0300

    Update scripts.js

--- themes/gsgsite/assets/js/scripts.js
+++ themes/gsgsite/assets/js/scripts.js
@@ -1,2 +1,12 @@
 jQuery(document).ready(function ($) {
-  });
+
+
+$('.mainNavigationContainer .menu-item-has-children > a, .fullscreenMenu .menu-item-has-children > a').click(function (e) {
+  console.log('click and prev def');
+  e.preventDefault();
+  $(this).parent().toggleClass('active');
+  $(this).parent().find('.sub-menu').toggleClass('activeMenu');
+  $('.mainNavigationContainer .activeLine').toggleClass('hide');
+});
+
+});
