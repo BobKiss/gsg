d5f5fdef31e624f74c979fb4fcbbfb0584ca3bf0
Anatolii Serbit <outlawcybercrimes@gmail.com>
2019-09-26 18:59:41 +0300

    Create HouseSystem.php

---
+++ themes/gsgsite/templates/HouseSystem.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Template Name: House System
+*/
+?>
+
+<?php get_header(); ?>
+<section id="housesys" class="housesys">
+			<div class="container-fluid">
+				<div class="row no-gutters">
+						<div class="housesys__img">
+							<img src="<?php bloginfo('template_url'); ?>/assets/images/r2house.png">
+							<div class="housesys__menu_nav">
+								<a href="#" class="housesys__menu_lnk"><img src="<?php bloginfo('template_url'); ?>/assets/images/arrow-left.png"></a>
+								<a href="#" class="housesys__menu_lnk"><img src="<?php bloginfo('template_url'); ?>/assets/images/arrow-right.png"></a>
+								<a href="#" class="housesys__menu_lnk"><img src="<?php bloginfo('template_url'); ?>/assets/images/plus.png"></a>
+								<a href="#" class="housesys__menu_lnk"><img src="<?php bloginfo('template_url'); ?>/assets/images/minus.png"></a>
+								<a href="#" class="housesys__menu_lnk"><img src="<?php bloginfo('template_url'); ?>/assets/images/download.png"></a>
+							</div>
+						</div>
+				
+						<div class="housesys__menu">
+							<div class="housesys__menu_wrap">
+								<h6 class="housesys__menu_title">בחר נכס</h6>
+								<div class="housesys__menu_desc">
+									<div><span class="housesys__menu_count">122</span><p class="housesys__menu_text">בחר מגרש</p></div>
+									<div><span class="housesys__menu_count">חזית</span><p class="housesys__menu_text">בחר תצוגת נכס</p></div>
+									<div><span class="housesys__menu_count">ללא</span><p class="housesys__menu_text">בחר קומה</p></div>
+									<div><span class="housesys__menu_count">ללא</span><p class="housesys__menu_text">בחר דירה</p></div>
+									<div><span class="housesys__menu_count">ללא</span><p class="housesys__menu_text">בחר חבילה</p></div>
+								</div>
+								<a class="housesys__menu_btn" href="#">הצג שרטוט נכס</a>
+								<a class="housesys__menu_btn" href="#"> הצג מפרט ומחירון</a>
+								<a class="housesys__menu_btn" href="#">הצג תמונות והדמיות</a>
+								<a class="housesys__menu_btn" href="#">הצג חבילות קונספט</a>
+								<div class="housesys__menu_desc">
+									<div><span class="housesys__menu_count">0</span><p class="housesys__menu_text">מחיר בסיס</p></div>
+									<div><span class="housesys__menu_count">0</span><p class="housesys__menu_text">תוספת חבילה</p></div>
+									<div><span class="housesys__menu_count">0</span><p class="housesys__menu_text">קוד הנחה</p></div>
+									<div><span class="housesys__menu_count">0</span><p class="housesys__menu_text">מחיר סופי </p></div>
+									<div><span class="housesys__menu_count">0</span><p class="housesys__menu_text">סה”כ דמי הרשמה</p></div>
+								</div>
+								<a class="housesys__menu_btn" href="#">לטופס הרשמה ותשלום דמי הרשמה ></a>
+								
+							</div>
+							
+						</div>
+					
+				</div>
+			</div>			
+	</section>
+	<!-- #sectionHousesys -->
+<?php wp_footer(); ?>
+
+
+
