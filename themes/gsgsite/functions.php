b87935f80e3bb9c8eed5745b1d9563fcd22893b5
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-10-01 12:28:26 +0300

    up

--- themes/gsgsite/functions.php
+++ themes/gsgsite/functions.php
@@ -127,6 +127,7 @@ function gsgsite_scripts() {
 	if (get_locale() === 'he_IL') {
 		wp_enqueue_style( 'main-style-hebrew', get_template_directory_uri() . '/assets/css/rtl.css' );
 	}
+	wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' );
 	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/css/main.css' );
 	wp_enqueue_style( 'main-style-alex', get_template_directory_uri() . '/assets/css/main_alex.css' );
 	wp_enqueue_style( 'projects', get_template_directory_uri() . '/assets/css/projects.css' );
@@ -135,7 +136,6 @@ function gsgsite_scripts() {
 	wp_enqueue_style( 'Assistant-font', 'https://fonts.googleapis.com/css?family=Assistant:300,400,600,700,800&display=swap&subset=hebrew' );
 	wp_enqueue_style( 'Dosis', 'https://fonts.googleapis.com/css?family=Dosis:400,700,800' );
 	wp_enqueue_style( 'slick-slider', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css' );
-	wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' );
 
 	wp_enqueue_style( 'fawesome', get_template_directory_uri() . '/assets/fonts/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css' );
 
--- themes/gsgsite/templates/ImmediateProjects.php
+++ themes/gsgsite/templates/ImmediateProjects.php
@@ -7,7 +7,24 @@ Template Name: Immediate Projects
 
 <div class="page after-header-padding">
 
+  <div class="big-projects-list container-1">
+    <?php for($i = 0; $i < 4; $i++) : ?>
+      <div class="list-item">
+        <div class="item-desc">
+          <div class="item-bg" style="background-image: url('/wp-content/themes/gsgsite/assets/images/projects/placeholder1.jpg')">
+            <div class="item-info">
+              <div class="title">ברקן מול הים | ברקן</div>
+              <div class="excerpt">פרויקט ברקן מול הים, הינו פרויקט הרחבה קהילתית הכולל 62 מגרשים בגודל של כחצי דונם המיועדים לבניית וילות צמודות קרקע. הפרויקט ממוקם באזור הצפון מערבי של הישוב (הגבעה הצפונית) ופונה לנוף הררי מרהיב. נשארו 3 מגרשים אחרונים בלבד!</div>
+              <a href="#" class="view-link">למידע נוסף אודות הפרוייקט לחץ כאן ></a>
+              <div class="view-btn">הצג פרוייקט</div>
+            </div>
+          </div>
+        </div>
+        <div class="item-meta">ברקן מול הים | ברקן</div>
 
+      </div>
+    <?php endfor; ?>
+  </div>
 </div>
 
 <?php
