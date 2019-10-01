51a3002a35b064f508870e71ce85089e69fc89f6
Dogrof <Zeron1@bk.ru>
2019-10-01 17:41:05 +0300

    123

--- themes/gsgsite/templates/ImmediateProjects.php
+++ themes/gsgsite/templates/ImmediateProjects.php
@@ -6,25 +6,63 @@ Template Name: Immediate Projects
 <?php get_header(); ?>
 
 <div class="page after-header-padding">
-
   <div class="big-projects-list container-1">
-    <?php for($i = 0; $i < 4; $i++) : ?>
-      <div class="list-item">
-        <div class="item-desc">
-          <div class="item-bg" style="background-image: url('/wp-content/themes/gsgsite/assets/images/projects/placeholder1.jpg')">
-            <div class="item-info">
-              <div class="title">ברקן מול הים | ברקן</div>
-              <div class="excerpt">פרויקט ברקן מול הים, הינו פרויקט הרחבה קהילתית הכולל 62 מגרשים בגודל של כחצי דונם המיועדים לבניית וילות צמודות קרקע. הפרויקט ממוקם באזור הצפון מערבי של הישוב (הגבעה הצפונית) ופונה לנוף הררי מרהיב. נשארו 3 מגרשים אחרונים בלבד!</div>
-              <a href="#" class="view-link">למידע נוסף אודות הפרוייקט לחץ כאן ></a>
-              <div class="view-btn">הצג פרוייקט</div>
+  <?php
+    if( have_rows('project_items_repeater') ):
+        while ( have_rows('project_items_repeater') ) : the_row();
+?>
+
+        <div class="list-item">
+          <div class="item-desc">
+            <div class="item-bg" style="background-image: url('<?php the_sub_field('project_image'); ?>')">
+              <div class="item-content-block">
+                <?php if (get_sub_field('project_name') && get_sub_field('project_description') ): ?>
+                  <div class="item-info">
+                    <div class="title"><?php the_sub_field('project_name'); ?></div>
+                    <div class="excerpt"><?php the_sub_field('project_description'); ?></div>
+                    <div class="view-link"><?php _e('For more information about the project, click here','Gsg'); ?> > </div>
+                  </div>
+
+                <?php endif; ?>
+                <a href="<?php the_sub_field('project_link'); ?>" class="view-btn"><p class="btn-content"><?php _e('View project','Gsg'); ?></p></a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="item-meta">ברקן מול הים | ברקן</div>
-
+          <div class="item-meta"><?php the_sub_field('project_name'); ?></div>
       </div>
-    <?php endfor; ?>
+        <?php
+        endwhile;
+    else :
+    endif;
+  ?>
+
   </div>
+<div class="small-projects-list container-2">
+  <marquee scrollamount="5" class="contactus__lets"><?php _e('Sold Out Projects - Sold Out Projects - Sold Out Projects','Gsg'); ?></marquee>
+  <?php
+    if( have_rows('mini_projects_repeater') ):
+        while ( have_rows('mini_projects_repeater') ) : the_row();
+?>
+        <div class="list-item">
+          <div class="item-desc">
+            <div class="item-bg" style="background-image: url('<?php the_sub_field('project_image'); ?>')">
+              <div class="item-info">
+                <div class="title"><?php the_sub_field('project_name'); ?></div>
+                  <div class="excerpt"><?php the_sub_field('project_description'); ?></div>
+                <a href="<?php the_sub_field('project_link'); ?>" class="view-btn"><?php _e('More info','Gsg'); ?></a>
+              </div>
+            </div>
+          </div>
+      </div>
+        <?php
+        endwhile;
+    else :
+    endif;
+  ?>
+</div>
+
+
+
 </div>
 
 <?php
---
+++ themes/gsgsite/templates/LongtermProjects.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Long-Term Projects
+*/
+?>
+<?php get_header(); ?>
+
+<div class="page after-header-padding">
+
+  <div class="big-projects-list container-1">
+      <div class="list-item">
+        <div class="item-desc">
+          <div class="item-bg" style="background-image: url('/wp-content/themes/gsgsite/assets/images/projects/placeholder1.jpg')">
+            <div class="item-info">
+              <div class="title">ברקן מול הים | ברקן</div>
+              <div class="excerpt">פרויקט ברקן מול הים, הינו פרויקט הרחבה קהילתית הכולל 62 מגרשים בגודל של כחצי דונם המיועדים לבניית וילות צמודות קרקע. הפרויקט ממוקם באזור הצפון מערבי של הישוב (הגבעה הצפונית) ופונה לנוף הררי מרהיב. נשארו 3 מגרשים אחרונים בלבד!</div>
+              <a href="#" class="view-link">למידע נוסף אודות הפרוייקט לחץ כאן ></a>
+              <div class="view-btn">הצג פרוייקט</div>
+            </div>
+          </div>
+        </div>
+        <div class="item-meta">ברקן מול הים | ברקן</div>
+
+      </div>
+  </div>
+</div>
+
+<?php
+get_footer();
+?>
