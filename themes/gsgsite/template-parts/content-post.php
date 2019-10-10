ef30505371df32f716f57d95a53b7d22c6031c48
Dogrof <Zeron1@bk.ru>
2019-10-10 20:31:50 +0300

    Single page

---
+++ themes/gsgsite/template-parts/content-post.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package gsgsite
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header id="header" class="header" style="background: url(<?php echo site_url(); ?>/wp-content/themes/gsgsite/assets/images/archive_01.jpg) center center / cover">
+	<div class="container single-post">
+		<div class="row">
+			<div class="col-12">
+				<div class="blog__borderblock"></div>
+				<div class="blog__borderblock"><h2 class="header__title">
+					 איך מתמודדים \n
+	עם דייר סרבן?
+				</h2></div>
+				<div class="blog__borderblock">
+						<img class="decorCircle" src="<?php echo site_url() ?>/wp-content/themes/gsgsite/assets/images/blogcirc.png" alt="blogcircle">
+				</div>
+			</div>
+		</div>
+	</div>
+	</header> <!-- headerSection-->
+
+
+
+
+	<section id="blogpage" class="blogpage">
+		<div class="container">
+
+
+			<div class="entry-content">
+				<h4 class="blogpage__title">איך מתמודדים עם דייר סרבן ?  תמ”א 38
+					<span>מאת מעיין בכר
+					</span>
+				</h4>
+				<?php
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gsgsite' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+					) );
+
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gsgsite' ),
+						'after'  => '</div>',
+					) );
+					?>
+					<div class="blog__nav">
+						<div class="nav-link-single prev"><?php previous_post_link( '%link', '< לכתבה הקודמת' ); ?></div>
+						<div class="nav-link-single center"><a href="" class="blog__nav_text">חזרה לעמוד מגזין</a></div>
+						<div class="nav-link-single next"><?php next_post_link( '%link', 'לכתבה הבאה >' ); ?></div>
+
+					</div>
+				</div><!-- .entry-content -->
+		</div>
+	</section>
+
+</article><!-- #post-<?php the_ID(); ?> -->
