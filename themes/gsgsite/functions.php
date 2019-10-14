963ea9af12243a07807b75f202a0d7c213169964
Ancodz <outlawcybercrimes@gmail.com>
2019-10-14 06:54:56 +0300

    singleprodFix

--- themes/gsgsite/functions.php
+++ themes/gsgsite/functions.php
@@ -45,6 +45,7 @@ if ( ! function_exists( 'gsgsite_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'gsgsite' ),
+			'menu-2' => esc_html__( 'Flat', 'gsgsite' )
 		) );
 
 		/*
